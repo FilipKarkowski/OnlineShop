5c057839316ea2400466c8093a7e4b6821817c64
Filip Karkowski <114432611+Xeoaxis@users.noreply.github.com>
2024-04-15 14:06:20 +0200

    Project setup

---
+++ OnlineShopBackend/src/main/java/FK/PROJ/OnlineShopBackend/App.java
@@ -0,0 +1,13 @@
+package FK.PROJ.OnlineShopBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
